9375d83b5a8aa15ac630934b2a68cfff88ce9dc9
TheMenino <themenino002@gmail.com>
2018-04-21 19:33:32 -0300

    v1.0b

---
+++ Module1.vb
@@ -0,0 +1,135 @@
+﻿Imports System.Runtime.InteropServices
+Imports System.Text
+Imports System.Net
+Imports System.Security.Principal
+Imports System.Threading
+
+Module Module1
+
+    Function RemoveWhitespace(fullString As String) As String
+        Return New String(fullString.Where(Function(x) Not Char.IsWhiteSpace(x)).ToArray())
+    End Function
+
+
+    Sub Main()
+        Console.ForegroundColor = ConsoleColor.Magenta
+        Console.Title = "Loading..."
+        Console.Write("Loading... ")
+        Using barradosatanas = New ProgressBar
+            For i = 0 To 100
+                barradosatanas.Report(i / 100)
+                Thread.Sleep(50)
+            Next
+            Console.Clear()
+        End Using
+
+        Try
+            Dim oProcess As New Process()
+            Dim oStartInfo As New ProcessStartInfo("cmd.exe", "/c tasklist /v /fo csv | findstr /i Skype.exe")
+            oStartInfo.UseShellExecute = False
+            oStartInfo.CreateNoWindow = True
+            oStartInfo.RedirectStandardOutput = True
+            oProcess.StartInfo = oStartInfo
+            oProcess.Start()
+
+            Dim sOutput As String
+            Using oStreamReader As System.IO.StreamReader = oProcess.StandardOutput
+                sOutput = oStreamReader.ReadToEnd()
+            End Using
+
+            Dim username As String = ""
+            Try
+                username = sOutput.Split(",")(8).Split({""""c})(1).Split("- ")(1)
+            Catch ex As Exception
+                username = "Error"
+            End Try
+            Dim s1 As String() = sOutput.Split(",")
+            Dim s2 As String() = s1(1).Split({""""c})
+
+            Dim pid As String = sOutput.Split(",")(1).Split({""""c})(1)
+
+            Dim oProcess2 As New Process()
+            Dim oStartInfo2 As New ProcessStartInfo("cmd.exe", "/c netstat -ano | findstr UDP | findstr " + pid)
+            oStartInfo2.UseShellExecute = False
+            oStartInfo2.CreateNoWindow = True
+            oStartInfo2.RedirectStandardOutput = True
+            oProcess2.StartInfo = oStartInfo2
+            oProcess2.Start()
+
+            Dim sOutput2 As String
+            Using oStreamReader2 As System.IO.StreamReader = oProcess2.StandardOutput
+                sOutput2 = oStreamReader2.ReadToEnd()
+            End Using
+
+            For Each Line As String In sOutput2.Split(vbNewLine)
+                If Line.Contains("0.0.0.0") = False And Line.Contains("127.0.0.1") = False And Line.Contains("[::]") = False And String.IsNullOrWhiteSpace(Line) = False And Line.Contains(":443") = False Then
+                    Dim output1 As String = RemoveWhitespace(Line.Split(":")(0)).Replace("UDP", String.Empty)
+                    Dim output2 As String = RemoveWhitespace(Line.Split(":")(1).Replace("*", String.Empty))
+                    If username = "Error" Then
+                        Console.Title = "ERRO: Você pode estar em uma chamada já!"
+                        Console.ForegroundColor = ConsoleColor.White
+
+                        Console.WriteLine("SkyFall - Skype Sniffer v1.0b")
+                        Console.WriteLine(" ")
+                        Console.ForegroundColor = ConsoleColor.Magenta
+                        Console.WriteLine("ERRO: Você pode estar em uma chamada já!")
+                        Console.Read()
+                        End
+                    End If
+
+
+                    Console.Title = "SkyFall - Skype Sniffer v1.0b - Usuario: " + username + " - IPv4: " + output1 + " - Porta: " + output2
+                    Console.ForegroundColor = ConsoleColor.White
+
+                    Console.WriteLine("SkyFall - Skype Sniffer v1.0b")
+                    Console.WriteLine(" ")
+                    Console.ForegroundColor = ConsoleColor.Magenta
+                    Console.WriteLine("Usuario: " + username)
+                    Console.WriteLine("IPv4: " + GetIpV4())
+                    Console.WriteLine("Porta: " + output2)
+                    Console.WriteLine(" ")
+                    Console.ForegroundColor = ConsoleColor.White
+
+
+                    Console.WriteLine(" ")
+
+
+
+                    Console.ForegroundColor = ConsoleColor.Magenta
+
+
+                 
+                    [SN].IPV4 = GetIpV4()
+                    [SN].Porta = output2
+                    [SN].STARTED = True
+                    [SN].BindSocket()
+                    Console.Read()
+                End If
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Function GetIpV4() As String
+
+        Dim myHost As String = Dns.GetHostName
+        Dim ipEntry As IPHostEntry = Dns.GetHostEntry(myHost)
+        Dim ip As String = ""
+
+        For Each tmpIpAddress As IPAddress In ipEntry.AddressList
+            If tmpIpAddress.AddressFamily = Sockets.AddressFamily.InterNetwork Then
+                Dim ipAddress As String = tmpIpAddress.ToString
+                ip = ipAddress
+                Exit For
+            End If
+        Next
+
+        If ip = "" Then
+            Throw New Exception("No IP found!")
+        End If
+
+        Return ip
+
+    End Function
+
+End Module
---
+++ Skype.vb
@@ -0,0 +1,110 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Public Class [SN]
+
+    Public Shared SOCKETS As New Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP)
+    Public Shared BYTE_DATA(4096) As Byte
+    Public Shared STARTED As Boolean
+    Public Shared IP_FROM As IPAddress
+    Public Shared IP_TO As IPAddress
+    Public Shared DES_PORT As UInteger
+    Public Shared SRC_PORT As UInteger
+    Public Shared IPV4 As String
+    Private Shared SK As Threading.Thread
+    Public Shared Porta As Integer
+    Private Shared aTimer As New System.Timers.Timer
+
+
+    Public Shared Sub OnReceive(ByVal asyncresult As IAsyncResult)
+        If STARTED = True Then
+            Dim READ_LENGTH As UInteger = BitConverter.ToUInt16(Byteswap(BYTE_DATA, 0), 0)
+            SRC_PORT = BitConverter.ToUInt16(Byteswap(BYTE_DATA, 22), 0)
+            DES_PORT = BitConverter.ToUInt16(Byteswap(BYTE_DATA, 24), 0)
+
+            IP_FROM = New IPAddress(BitConverter.ToUInt32(BYTE_DATA, 12))
+            IP_TO = New IPAddress(BitConverter.ToUInt32(BYTE_DATA, 16))
+
+            ' SK = New Threading.Thread(AddressOf RESULT_UPDATE)
+            '  SK.IsBackground = True
+            ' SK.Start()
+            Try
+
+                '
+                Dim threadStart As New Threading.ThreadStart(AddressOf RESULT_UPDATE)
+                '
+                Dim thread As New Threading.Thread(threadStart)
+                thread.IsBackground = True
+
+                thread.Priority = Threading.ThreadPriority.Highest
+                thread.Start()
+            Catch ex As Exception
+
+
+            End Try
+
+
+        End If
+        SOCKETS.BeginReceive(BYTE_DATA, 0, BYTE_DATA.Length, SocketFlags.None, New AsyncCallback(AddressOf OnReceive), Nothing)
+    End Sub
+    Private Shared IPL As String
+    Private Shared IPList As List(Of String)
+
+
+    Public Shared Sub RESULT_UPDATE()
+
+        Do
+
+            Try
+
+
+                If BYTE_DATA(9) = 17 And IP_FROM.ToString = IPV4 And DES_PORT = 80 Or DES_PORT = 443 Or DES_PORT = 22 Or DES_PORT = 21 And IP_TO.ToString.StartsWith("104") = False Then
+
+
+                    If Not IPL = IP_TO.ToString Then
+                        If Not IP_TO.ToString = IPV4 Then
+
+
+                            IPL = IP_TO.ToString
+                            Console.WriteLine(GetIpV4() + ":" + SRC_PORT.ToString + " ~> " + (String.Format("{0}{1}", IP_TO.ToString, ":" & DES_PORT, vbCrLf)))
+
+                        End If
+
+
+                    End If
+                End If
+
+
+                '  SK = New Threading.Thread(AddressOf RESULT_UPDATE)
+                '   SK.IsBackground = True
+                '  SK.Start()
+                Threading.Thread.Sleep(7000)
+            Catch ex As Exception
+
+            End Try
+
+        Loop
+    End Sub
+
+    Public Shared Function Byteswap(ByVal bytez() As Byte, ByVal index As UInteger)
+        Dim result(1) As Byte
+        result(0) = bytez(index + 1)
+        result(1) = bytez(index)
+        Return result
+    End Function
+
+    Public Shared Sub BindSocket()
+
+        Try
+            SOCKETS.Bind(New IPEndPoint(System.Net.IPAddress.Parse(IPV4), Porta))
+            SOCKETS.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, True)
+            Dim bytrue() As Byte = {1, 0, 0, 0}
+            Dim byout() As Byte = {1, 0, 0, 0}
+            SOCKETS.IOControl(IOControlCode.ReceiveAll, bytrue, byout)
+            SOCKETS.Blocking = False
+
+            ReDim BYTE_DATA(SOCKETS.ReceiveBufferSize)
+            SOCKETS.BeginReceive(BYTE_DATA, 0, BYTE_DATA.Length, SocketFlags.None, New AsyncCallback(AddressOf OnReceive), Nothing)
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
